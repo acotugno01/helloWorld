e4ca0f0d29f97d393404b68d9536c45c6de09e58
tonycot <acotugno@terpmail.umd.edu>
2023-02-20 21:09:32 -0500

    commit to app.py for the html file about me

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def hello():  # put application's code here
     return render_template ('hello.html')
 
 @app.route('/about')
-def about():  # put application's code here
+def abouts():  # put application's code here
     return render_template ('about.html')
 
 if __name__ == '__main__':
