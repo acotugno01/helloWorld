dba4419cf11ecfeae3c0342c71d166dbd35b907a
tonycot <acotugno@terpmail.umd.edu>
2023-02-20 21:28:15 -0500

    commit app.py about me for 5th time because link error

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def hello():  # put application's code here
     return render_template ('hello.html')
 
 @app.route('/about')
-def about4():  # put application's code here
+def about5():  # put application's code here
     return render_template ('about.html')
 
 if __name__ == '__main__':
