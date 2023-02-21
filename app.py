1b31e911e6a218bac9725bf1d60f1b0ff10afa2d
tonycot <acotugno@terpmail.umd.edu>
2023-02-20 20:50:41 -0500

    New route to about page created

--- app.py
+++ app.py
@@ -7,9 +7,14 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World! From Antonio Cotugno, I am adding my first code change'
 
+
 @app.route('/hello')
 def hello():  # put application's code here
     return render_template ('hello.html')
+
+@app.route('/about')
+def about():  # put application's code here
+    return render_template ('about.html')
 
 if __name__ == '__main__':
     app.run()
