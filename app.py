f06e831c71f020db2fdb1661eb2b0b11a4b4159c
tonycot <acotugno@terpmail.umd.edu>
2023-03-28 19:04:05 -0400

    steps 4-8

--- app.py
+++ app.py
@@ -21,9 +21,13 @@ def about():  # put application's code here
 def favorite_course():  # put application's code here
     return render_template ('favorite-course.html')
 
-@app.route('/contact', methods = ['GET', 'POST'])
-def contact():  # put application's code here
-    return render_template ('contact.html')
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run()
