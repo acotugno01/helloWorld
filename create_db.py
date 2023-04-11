3bd5cc3785b5b4a07e10363c6627460ceaacdab6
tonycot <acotugno@terpmail.umd.edu>
2023-04-10 20:41:05 -0400

    added all new files

--- create_db.py
+++ create_db.py
@@ -22,7 +22,9 @@ with app.app_context():
         {'username': 'manager', 'email': 'manager@umd.edu', 'first_name':'Joe', 'last_name':'King',
             'password': generate_password_hash('managerpw', method='sha256'), 'role':'MANAGER'},
         {'username': 'admin', 'email': 'admin@umd.edu', 'first_name':'Crystal', 'last_name':'Ball',
-            'password': generate_password_hash('adminpw', method='sha256'), 'role':'ADMIN'}
+            'password': generate_password_hash('adminpw', method='sha256'), 'role':'ADMIN'},
+        {'username': 'acotugno', 'email': 'acotugno@umd.edu', 'first_name':'Antonio', 'last_name':'Cotugno',
+            'password': generate_password_hash('acotugno', method='sha256'), 'role':'STUDENT'}
     ]
 
     for each_user in users:
